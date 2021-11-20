008858503dde98854c986c0a6cb5e8da3c8c4b61
Andrei <kalmyq@gmail.com>
2021-11-20 15:12:51 +0400

    init

---
+++ stepik/lesson329.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import math
+
+#def test_input_text(expected_result, actual_result):
+
+expected_result = 8
+actual_result = 11
+assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+
+
+
+
